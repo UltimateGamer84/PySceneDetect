505c1ddb1af7e35ddacb3dffb22f77dd708ad392
Riad <combatarms970711@gmail.com>
2019-11-30 20:04:35 -0500

    Fixed space

--- scenedetect/detectors/poly_detector.py
+++ scenedetect/detectors/poly_detector.py
@@ -7,8 +7,7 @@ from scenedetect.scene_detector import SceneDetector
 
 
 class PolyDetector(SceneDetector):
-    
-     def __init__(self):
-         super(PolyDetector, self).__init__()
-         self._metric_keys = ['delta_example']
-         self.cli_name = 'detect-poly'
+    def __init__(self):
+        super(PolyDetector, self).__init__()
+        self._metric_keys = ['delta_example']
+        self.cli_name = 'detect-poly'
